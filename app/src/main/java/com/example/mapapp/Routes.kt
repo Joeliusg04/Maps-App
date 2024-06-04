63abcb7267564d63b902b391eb0bd8f6fcae306f
Joel <joelgarciag04@gmail.com>
2024-06-04 17:06:50 +0200

    Final commit

--- app/src/main/java/com/example/mapapp/Routes.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mapapp
-
-sealed class Routes(val route: String) {
-    object Splash: Routes("splash")
-    object Pantalla2: Routes("pantalla2")
-    object Pantalla3: Routes("pantalla3")
-}
